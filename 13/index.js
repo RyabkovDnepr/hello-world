609aba6560f83f49c7556f19c247ed886d843536
RyabkovDnepr <37886065+RyabkovDnepr@users.noreply.github.com>
2020-01-09 10:50:00 +0200

    appendFileSync is done

---
+++ 13/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+let text = 'some kind of text\n';
+fs.appendFileSync('file_for_text.txt', text);
+text = 'other kind of text\n';
+fs.appendFileSync('file_for_text.txt', text);
+text = 'another kind of text\n';
+fs.appendFileSync('file_for_text.txt', text);
