1928c5a96c067453b099bacc2fbe0f1738a2cf15
RyabkovDnepr <37886065+RyabkovDnepr@users.noreply.github.com>
2020-01-09 10:57:55 +0200

    Update index.js

--- 13/index.js
+++ 13/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 let text = 'some kind of text\n';
-fs.appendFileSync('file_for_text.txt', text);
+fs.appendFileSync('fileForText.txt', text);
 text = 'other kind of text\n';
-fs.appendFileSync('file_for_text.txt', text);
+fs.appendFileSync('fileForText.txt', text);
 text = 'another kind of text\n';
-fs.appendFileSync('file_for_text.txt', text);
+fs.appendFileSync('fileForText.txt', text);
