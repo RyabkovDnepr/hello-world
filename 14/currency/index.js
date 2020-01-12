50c9af368d93dcc2ff8c857ecc4fa8bc9a86f48c
RyabkovDnepr <37886065+RyabkovDnepr@users.noreply.github.com>
2020-01-12 21:50:53 +0200

    Update index.js

--- 14/currency/index.js
+++ 14/currency/index.js
@@ -2,7 +2,7 @@ let fs = require('fs');
 let request =  require('request');
 
 
-request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3&fbclid=IwAR20GSdKClDe5lvU3MiXPygbLhU0DPz_Oi6npoZ7W6XM72oSPdhmZpMI7bE',
+request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3',
     function (error, response, body) {
     //console.log('error:', error); // Print the error if one occurred
     //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
